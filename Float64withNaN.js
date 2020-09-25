814bf562711fdc93a74f38f1e3fdbcc0a494967d
RYaoYao <ryanyao13@gmail.com>
2020-09-25 17:51:47 +0800

    Added all existing files

---
+++ Float64withNaN.js
@@ -0,0 +1,184 @@
+const hex_index = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+// Add trailing given character(s) on the given side of the
+// given string to achieve the given length.
+function Pad(str, length = 11, chr = '0', left = true) {
+	str = str.toString();
+	
+	if (str.length >= length)
+		return str;
+	
+	chr = chr.repeat(length - str.length);
+	
+	return left ? chr + str : str + chr;
+}
+// Base 10 to base 2. Also accomodates decimals (eg. 12.34).
+function DecToBin(num, limit = 20) {
+	if (num == 0)
+		return '0';
+	
+	let str = '';
+	let whl = Math.floor(num);
+	let dec = num - whl;
+	
+	while (whl > 0) {
+		str = (whl % 2) + str;
+		whl = Math.floor(whl / 2);
+	}
+	
+	if (dec > 0) {
+		str += '.';
+		
+		for (let i = 0; dec > 0 && i < limit; i++) {
+			dec *= 2;
+			str += dec >= 1 ? '1' : '0';
+			dec %= 1;
+		}
+	}
+	
+	return str;
+}
+// Base 2 to base 10.
+// Automatically converted to integer.
+function BinToDec(bin) {
+	let num = 0;
+
+	for (let i = 0; i < bin.length; i++)
+		if (bin[bin.length - i - 1] === '1')
+			num += 2 ** i;
+		
+	return num;
+}
+
+// Base 2 to base 16. Also accomodates decimals (eg. 101.01).
+function BinToHex(bin) {
+	let a = bin;
+	let b = '';
+	
+	if (bin.includes('.')) {
+		let arr = bin.split('.');
+		a = arr[0];
+		b = arr[1];
+	}
+	
+	let an = a.length % 4;
+	let bn = b.length % 4;
+	
+	if (an != 0)
+		a = '0'.repeat(4 - an) + a;
+	
+	if (bn != 0)
+		b = '0'.repeat(4 - bn) + b;
+	
+	let res = BinToHex_Internal(a);
+	
+	if (b.length > 0)
+		res += '.' + BinToHex_Internal(b);
+	
+	return res;
+}
+
+function BinToHex_Internal(bin) {
+	if (bin.length == 0)
+		return '';
+	
+	return bin.match(/.{1,4}/g).map(v => hex_index[BinToDec(v)]).join('');
+}
+
+// Print result as object.
+function Float64Result(sign, exponent, mantissa) {
+	return {
+		binary_representation: `${sign} ${exponent} ${mantissa}`,
+		hex: BinToHex(sign + exponent + mantissa)
+	}
+}
+
+
+// Float 32 to binary representation with hex.
+// Example; -101.01x2^5 = Float64ToBin(1, '101', '01', 5)
+// sign: 0 = positive; 1 = negative.
+// bin: binary
+// pow: power (2^n)
+// overflowType: 0 = infinity; 1 = signaling NaN; 2 = quiet NaN
+
+function Float64(sign, bin, pow, overflowType = 0) {
+	// Special Case: Zero
+	if (bin === '0')
+		return Float64Result(sign, '00000000000', '0000000000000000000000000000000000000000000000000000');
+	
+	
+	// Separate whole number and decimal.
+	
+	let a = bin;
+	let b = '';
+	
+	if (bin.includes('.')) {
+		let v = bin.split('.');
+		a = v[0];
+		b = v[1];
+	}
+	
+	
+	// Calculate E prime.
+
+
+	let e = Math.min(1023 + parseInt(pow) + a.length - 1, 2047);
+	console.log(e);
+	
+	if (e == 2047) {
+		switch (overflowType) {
+			case 0:
+				// Special Case: Infinity
+				return Float64Result(sign, '11111111111', '0000000000000000000000000000000000000000000000000000');
+				break;
+				
+			case 1:
+				// Special Case: Signaling NaN
+				a = '0' + a;
+				break;
+				
+			case 2:
+				// Special Case: Quiet NaN
+				a = '1' + a;
+		}
+	}
+	
+	
+	// Special Case: Denormalized
+	
+	if (e <= 0) {
+		a = '0'.repeat(1 - e) + a;
+		e = 0;
+	}
+	
+	
+	// Calculate exponent and mantissa.
+	
+	let exponent = Pad(DecToBin(e));
+	let mantissa = Pad((a.substr(1) + b).substr(0, 52), 52, '0', false);
+	
+	
+	// Print result.
+	
+	return Float64Result(sign, exponent, mantissa);
+}
+
+function convert(){
+	var inputnum = document.getElementById('inputnum');
+	var inputpow = document.getElementById('inputpow');
+	var bindisp = document.getElementById('bindisp');
+	var hexdisp = document.getElementById('hexdisp');
+	
+	var sign = 0;
+	console.log(inputnum.value)
+	if (inputnum.value != '') {
+		if (inputnum.value.includes("-"))
+			sign = 1;
+
+		var res = Float64(sign,inputnum.value,inputpow.value);
+
+		bindisp.innerHTML = res.binary_representation;
+		hexdisp.innerHTML = res.hex;
+	}
+}
+
